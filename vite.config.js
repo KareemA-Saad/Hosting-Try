b5775fc327fcde0dfb620e1ec68a9df61a7d9397
KareemA-Saad <kareemahmedsaad1999@gmail.com>
2025-06-14 23:29:54 +0300

    trial2wo

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    build: {
+        outDir: '.',
+        rollupOptions: {
+            input: {
+                main: './index.html',
+                shop: './shop.html',
+                cart: './cart.html',
+                category: './category.html',
+                dashboard: './dashboard.html',
+                history: './history.html',
+                productDetails: './productDetails.html',
+                user: './user.html'
+            }
+        }
+    },
+    server: {
+        port: 3000
+    }
+}) 
